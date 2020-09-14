a960d26b49d4983e8c80825522b11ee55b617ee3
kurbezz <kurbanovbul@gmail.com>
2020-09-14 13:29:37 +0300

    Code refactor, bug fix

--- source/sender.py
+++ source/sender.py
@@ -1,14 +1,11 @@
-from asyncio import run, create_subprocess_shell, set_event_loop_policy, wait_for, gather, Queue
+from asyncio import run, gather, Queue
 from asyncio import TimeoutError as ATimeoutError
 
-from typing import List, Dict
-from os import remove
-from os.path import abspath, exists
 from io import BytesIO
 
 import aiogram
-from aiohttp import request, ClientSession
-from telethon import TelegramClient, connection, errors
+from aiohttp import request
+from telethon import TelegramClient, errors
 import transliterate
 import asyncpg
 
@@ -16,22 +13,21 @@ from config import Config
 from db import FlibustaChannelDB
 
 
-async def normalize(book: "Book", file_type: str) -> str:  # remove chars that don't accept in Telegram Bot API
-    filename = '_'.join([a.short for a in book.authors]) + '_-_' if book.authors else ''
+# remove chars that don't accept in Telegram Bot API
+async def normalize(book: "Book", file_type: str) -> str:
+    filename = '_'.join([a.short for a in book.authors]) + \
+        '_-_' if book.authors else ''
     filename += book.title if book.title[-1] != ' ' else book.title[:-1]
     filename = transliterate.translit(filename, 'ru', reversed=True)
 
     for c in "(),….’!\"?»«':":
         filename = filename.replace(c, '')
 
-    for c, r in (('—', '-'), ('/', '_'), ('№', 'N'), (' ', '_'), ('–', '-'), ('á', 'a'), (' ', '_')):
+    for c, r in (('—', '-'), ('/', '_'), ('№', 'N'), (' ', '_'), ('–', '-'),
+                 ('á', 'a'), (' ', '_')):
         filename = filename.replace(c, r)
 
     return filename + '.' + file_type
-
-
-class NoContent(Exception):
-    pass
 
 
 class Author:
@@ -71,7 +67,8 @@ class Author:
 
     @property
     def books(self):
-        return [Book(x) for x in self.obj["books"]] if self.obj.get("books", None) else []
+        return [Book(x) for x in self.obj["books"]] \
+            if self.obj.get("books", None) else []
 
     @property
     def normal_name(self) -> str:
@@ -133,14 +130,16 @@ class Book:
 
     @property
     def authors(self):
-        return [Author(a) for a in self.obj["authors"]] if self.obj.get("authors", None) else None
+        return [Author(a) for a in self.obj["authors"]] \
+            if self.obj.get("authors", None) else None
 
     @property
     def caption(self) -> str:
         if not self.authors:
             return "📖 " + self.title
 
-        result = "📖 " + self.title + '\n\n' + '\n'.join(["👤 " + author.normal_name for author in self.authors])
+        result = "📖 " + self.title + '\n\n' + \
+            '\n'.join(["👤 " + author.normal_name for author in self.authors])
 
         if len(result) <= 1024:
             return result
@@ -148,17 +147,21 @@ class Book:
         i = len(self.authors)
         while len(result) > 1024:
             i -= 1
-            result = "📖 " + self.title + '\n\n' + '\n'.join(["👤 " + author.normal_name for author in self.authors[:i]]) + "\n и т.д."
+            result = "📖 " + self.title + '\n\n' + \
+                '\n'.join(
+                    ["👤 " + author.normal_name for author in self.authors[:i]]
+                ) + "\n и т.д."
         return result
-
 
     @staticmethod
     async def get_by_id(book_id: int) -> "Book":
-        async with request("GET", f"{Config.FLIBUSTA_SERVER_HOST}/book/{book_id}") as response:
-            if response.status == 204:
-                raise NoContent
+        async with request(
+            "GET",
+            f"{Config.FLIBUSTA_SERVER_HOST}/book/{book_id}"
+        ) as response:
+            if response.status != 200:
+                raise Exception(response.status)
             return Book(await response.json())
-
 
 
 class Sender:
@@ -170,8 +173,10 @@ class Sender:
     flibusta_channel_server_pool: asyncpg.pool.Pool
 
     def __init__(self):
-        self.client = TelegramClient(Config.SESSION, Config.APP_ID, Config.API_HASH)
-        
+        self.client = TelegramClient(
+          Config.SESSION, Config.APP_ID, Config.API_HASH
+        )
+
         self.bot = aiogram.Bot(token=Config.BOT_TOKEN)
 
     async def prepare(self):
@@ -192,16 +197,25 @@ class Sender:
 
         self.all_task_added = False
 
-        self.flibusta_server_pool = await asyncpg.create_pool(user=Config.FLIBUSTA_SERVER_DB_USER, password=Config.FLIBUSTA_SERVER_DB_PASSWORD,
-                                                              database=Config.FLIBUSTA_SERVER_DB_DATABASE, host=Config.FLIBUSTA_SERVER_DB_HOST)
-        
-        await FlibustaChannelDB.prepare(None)
+        self.flibusta_server_pool = await asyncpg.create_pool(
+            user=Config.FLIBUSTA_SERVER_DB_USER,
+            password=Config.FLIBUSTA_SERVER_DB_PASSWORD,
+            database=Config.FLIBUSTA_SERVER_DB_DATABASE,
+            host=Config.FLIBUSTA_SERVER_DB_HOST
+        )
 
+        await FlibustaChannelDB.prepare(None)
 
     async def upload(self, book_id: int, file_type: str):
         print(f"Download {book_id} {file_type}...")
         try:
-            async with request("GET", f"{Config.FLIBUSTA_SERVER_HOST}/book/download/{book_id}/{file_type}") as response:
+            async with request(
+                "GET",
+                (
+                    f"{Config.FLIBUSTA_SERVER_HOST}/book/download/"
+                    f"{book_id}/{file_type}"
+                )
+            ) as response:
                 content = await response.content.read()
         except ATimeoutError:
             return
@@ -214,14 +228,24 @@ class Sender:
         client = self.client
 
         try:
-            book_msg = await client.send_file(self.channel_dialog, file=data, caption=book_info.caption)
+            book_msg = await client.send_file(
+                self.channel_dialog,
+                file=data,
+                caption=book_info.caption
+            )
 
-            await FlibustaChannelDB.set_message_id(book_id, file_type, book_msg.id)
+            await FlibustaChannelDB.set_message_id(
+                book_id,
+                file_type,
+                book_msg.id
+            )
         except (errors.FilePartsInvalidError, ValueError):
             pass
 
     async def tasks_add(self):
-        book_rows = await self.flibusta_server_pool.fetch("SELECT id, file_type FROM book ORDER BY id DESC;")
+        book_rows = await self.flibusta_server_pool.fetch(
+            "SELECT id, file_type FROM book ORDER BY id DESC;"
+        )
 
         self.all_task_added = False
 
@@ -229,14 +253,16 @@ class Sender:
             book_id = row["id"]
             file_type = row["file_type"]
 
-            uploaded_row = await FlibustaChannelDB.get_message_id(book_id, file_type)
+            uploaded_row = await FlibustaChannelDB.get_message_id(
+                book_id, file_type
+            )
 
             if not uploaded_row:
                 await self.tasks.put(self.upload(book_id, file_type))
                 if file_type == "fb2":
                     await self.tasks.put(self.upload(book_id, "epub"))
                     await self.tasks.put(self.upload(book_id, "mobi"))
-        
+
         self.all_task_added = True
 
     async def execute_tasks(self):
